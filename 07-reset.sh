349331854b86fa2cad03b189b44cab846ce2d4ea
WaddedMeat <waddedmeat@gmail.com>
2022-06-03 13:46:54 -0400

    Displays the commands executed

--- 07-reset.sh
+++ 07-reset.sh
@@ -6,8 +6,10 @@ Resetting
 
 EOF
 
+set -x
 docker-compose down -v > /dev/null 2>&1
 helm -n example-nextjs delete my-app > /dev/null 2>&1
 kubectl delete ns example-nextjs > /dev/null 2>&1
 rm -rf my-helm-app my-app
+set +x
 
